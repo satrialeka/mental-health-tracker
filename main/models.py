b39ad978f8d99dc3aaf616d81a4726863bf72a95
satria <satria.aleka@gmail.com>
2024-09-18 14:49:42 +0700

    tutorial 3 selesai

--- main/models.py
+++ main/models.py
@@ -2,8 +2,8 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class MoodEntry(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # tambahkan baris ini
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
     feelings = models.TextField()
